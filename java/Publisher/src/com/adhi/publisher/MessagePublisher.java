4aaa751002e35a04d9d9fae4576210ceb85d8831
chamilaadhi <cka.adhi@gmail.com>
2013-10-13 21:00:23 +0530

    modified to suit observable pattern

--- java/Publisher/src/com/adhi/publisher/MessagePublisher.java
+++ java/Publisher/src/com/adhi/publisher/MessagePublisher.java
@@ -10,72 +10,89 @@ import java.io.PrintWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Observable;
 
 
-public class MessagePublisher {
-	public static void main(String[] args) {
-		
+public class MessagePublisher extends Observable implements Runnable {
+	
+	private String body = "";
+	private String publishUrl = "http://127.0.0.1:8280/message";
+	private int count = 0;
 
-		String body = "test";
-		String pubUrl = "http://192.168.0.1:8080/Message" ;
-		URL url;
-		try {
-			url = new URL(pubUrl);
-			WorkerThread worker = new WorkerThread(url, body);
-			worker.run();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	private String data = null;
+
+	public String getBody() {
+		return body;
 	}
 
-	 static class WorkerThread extends Thread {
-		 
-		private URL url;
-		private String body;
-		private String data = null;
-		public WorkerThread(URL url, String body) {
-			// TODO Auto-generated constructor stub
-			 this.url = url;
-			 this.body = body;
-		}
-		 @Override
-		public void run() {
-			
-			try {
-				
-				  data = sendMessage(url, body);
-				  System.out.println("Incomming: " + data);
-				  
-			} catch (Exception e) {
-					  e.printStackTrace();
-			}
-		}
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getPublishUrl() {
+		return publishUrl;
+	}
+
+	public void setPublishUrl(String publishUrl) {
+		this.publishUrl = publishUrl;
+	}
+
+	
+	 @Override
+	public void run() {
 		
-		 private String sendMessage(URL url, String message) throws IOException{
-				URLConnection connection = url.openConnection();
-				connection.setDoInput(true);
-				connection.setDoOutput(true);
-
-				connection.connect();
-
-				OutputStream os = connection.getOutputStream();
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
-				String payload = message;
-				pw.write(payload);
-				pw.close();
-
-				InputStream is = connection.getInputStream();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-				String line = null;
-				StringBuffer sb = new StringBuffer();
-				while ((line = reader.readLine()) != null) {
-				    sb.append(line);
-				}
-				is.close();
-				String response = sb.toString();
-				return response;
+		try {
+			
+			  data = sendMessage(publishUrl, body);
+			  System.out.println("Incomming: " + data);
+			  
+		} catch (Exception e) {
+					setNotification(-1);
+				  e.printStackTrace();
+		}
+	}
+	
+	 private String sendMessage(String pubUrl, String message) throws IOException{
+		 	URL url = new URL(pubUrl);
+			URLConnection connection = url.openConnection();
+			connection.setDoInput(true);
+			connection.setDoOutput(true);
+			setNotification(10);
+			
+			connection.connect();
+			setNotification(20);
+			
+			OutputStream os = connection.getOutputStream();
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(os));
+			String payload = message;
+			pw.write(payload);
+			pw.close();
+			setNotification(70);
+			
+			InputStream is = connection.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+			String line = null;
+			StringBuffer sb = new StringBuffer();
+			while ((line = reader.readLine()) != null) {
+			    sb.append(line);
 			}
+			is.close();
+			String response = sb.toString();
+			setNotification(100);
+			
+			return response;
+		}
+
+
+	 
+	 private void setNotification(int num){
+		
+		 this.count = num;
+		 setChanged();
+		 notifyObservers();
 	 }
+	 public int getValue(){
+		 return this.count;
+	 }
+	 
 }
